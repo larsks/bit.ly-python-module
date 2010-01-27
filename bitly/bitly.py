633a09f866e0da41d4f045c3a388fe5e54371abc
Lars Kellogg-Stedman <lars@oddbit.com>
2010-01-26 21:07:01 -0500

    - added some documentation - minor code cleanup

--- bitly/bitly.py
+++ bitly/bitly.py
@@ -9,6 +9,16 @@ API_URL = 'http://api.bit.ly'
 API_VERSION = '2.0.1'
 
 class APIError (Exception):
+    '''Raised by BitLy instances in the event of errors returned by the 
+    bit.ly REST API.  An APIError instance provides the following
+    attributes:
+
+    - *code* -- The numeric error code returned by bit.ly.
+    - *message* -- The textual error message returned by bit.ly.
+    - *result* -- The BitLy object associated with this exception
+      (may be None).
+    '''
+
     def __init__ (self, code, message, result=None):
         super(APIError, self).__init__()
         self.errorCode = code
@@ -20,31 +30,56 @@ class APIError (Exception):
                 self.errorMessage)
 
 class BitLy (object):
+    '''BitLy is a wrapper over the bit.ly REST API 
+    (http://code.google.com/p/bitly-api/wiki/ApiDocumentation).  API
+    calls are generated dynamically by the ``__getattr__`` method, and
+    arbitrary keyword arguments are converted into URL parameters.
+
+    Example usage::
+
+    >>> api = bitly.BitLy(api_user, api_key)
+    >>> res = api.shorten(longUrl='http://github.com/larsks')
+    >>> print res['http://github.com/larsks']['shortUrl']
+    http://bit.ly/9KKBJH
+
+    '''
+
     api_version = API_VERSION
+    api_url     = API_URL
 
     def __init__ (self, api_user, api_key):
         self.api_user = api_user
         self.api_key = api_key
 
-        # check credentials
+        # Check credentials: this calls the errors() method, which
+        # will raise an exception if we have invalid credentials
+        # (or a bad URL, etc).
         res = self.errors()
-        if res['errorCode'] != 0:
-            raise APIError(res['errorCode'], res['errorMessage'])
 
-    def __getattr__ (self, f):
+    def _build_query_string(self, kwargs):
+        params = {
+                'version'   : self.api_version,
+                'login'     : self.api_user,
+                'apiKey'    : self.api_key,
+                }
+
+        params.update(kwargs)
+        return urllib.urlencode(params)
+
+    def __getattr__ (self, func):
+        '''Generates a function that calls *func* via the bit.ly
+        REST api.  Transforms any keyword arguments into URL 
+        paramters.
+
+        Returns a Python dictionary containing the result of the bit.ly 
+        method call.
+
+        Raise bitly.APIError on errors returned by bit.ly.'''
+
         def _ (**kwargs):
-            d = {
-                    'version'   : self.api_version,
-                    'login'     : self.api_user,
-                    'apiKey'    : self.api_key,
-                    }
-
-            d.update(kwargs)
-
-            url = '/'.join([API_URL, f])
-            query = urllib.urlencode(d)
-
-            fd = urllib.urlopen(url, query)
+            url = '/'.join([self.api_url, func])
+            query_string = self._build_query_string(kwargs)
+            fd = urllib.urlopen(url, query_string)
             res = json.loads(fd.read())
 
             if res['errorCode'] != 0:
@@ -52,11 +87,20 @@ class BitLy (object):
                         res['errorCode'],
                         res['errorMessage'],
                         res)
-            return res
+            elif not 'results' in res:
+                raise APIError(-1, 'Unexpected response from bit.ly.', res)
+
+            return res['results']
 
         return _
 
 def main():
+    '''Reads configuration from the [bitly] section of
+    ~/.bitly.  Returns a BitLy() object to the caller.  This is 
+    primarily during development -- if you load this module
+    from the command line with ``python -i bitly/bitly.py``, the
+    ``api`` object will be available to you for testing.'''
+
     from ConfigParser import ConfigParser
     cf = ConfigParser()
     cf.read(os.path.expanduser('~/.bitly'))
